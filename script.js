a845720f99aee3736093371dffd941c0785e0639
naoyuun <yui.a_a@icloud.com>
2026-07-24 14:48:08 +0900

    menu スマホ版OK

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const targets = document.querySelectorAll(".fadeUp");
 
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry=>{
-        if(entry.isIntersecting){
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
     });
 });
 
-targets.forEach(target=>observer.observe(target));
+/*nav */
+targets.forEach((target) => observer.observe(target));
+
+const hamburger = document.querySelector(".header__hamburger");
+const menu = document.querySelector(".header__nav");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("open");
+});
